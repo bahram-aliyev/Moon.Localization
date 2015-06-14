e6691d296423d24f71b6e525a1447a21dac8a852
Dusan Janosik <dusan@djanosik.cz>
2015-06-14 20:23:56 +0200

    Adds interface for resource dictionaries.

---
+++ src/Moon.Localization/IResourceDictionary.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Globalization;
+
+namespace Moon.Localization
+{
+    /// <summary>
+    /// Defines basic contract for resource dictionaries.
+    /// </summary>
+    public interface IResourceDictionary
+    {
+        /// <summary>
+        /// Gets the culture of the dictionary.
+        /// </summary>
+        CultureInfo Culture { get; }
+
+        /// <summary>
+        /// Gets a collection of localized values.
+        /// </summary>
+        IDictionary<string, string> Values { get; }
+    }
+}
