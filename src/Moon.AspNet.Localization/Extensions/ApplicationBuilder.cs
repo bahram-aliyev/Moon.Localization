154c46e104aa773d7a60c888b82a3574991460f9
Dušan Janošík <dusan@djanosik.cz>
2016-01-28 14:05:41 +0100

    Makes it possible to specify default culture when configuring OWIN / ASP.NET pipeline.

--- src/Moon.AspNet.Localization/Extensions/ApplicationBuilder.cs
+++ src/Moon.AspNet.Localization/Extensions/ApplicationBuilder.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 using Microsoft.AspNet.Http;
 using Moon;
 using Moon.AspNet.Localization;
@@ -17,7 +18,18 @@ namespace Microsoft.AspNet.Builder
         /// <param name="app">The application builder.</param>
         /// <param name="loader">The dictionary loader.</param>
         public static IApplicationBuilder UseLocalization(this IApplicationBuilder app, Action<DictionaryLoader> loader)
-            => app.UseLocalization(new PathString("/assets/resources.js"), loader);
+            => app.UseLocalization("en-US", loader);
+
+        /// <summary>
+        /// Sets culture used by the application according to Accept-Language header.
+        /// </summary>
+        /// <param name="app">The application builder.</param>
+        /// <param name="defaultCulture">
+        /// The default culture used when a dictionary for the current culture does not exist.
+        /// </param>
+        /// <param name="loader">The dictionary loader.</param>
+        public static IApplicationBuilder UseLocalization(this IApplicationBuilder app, string defaultCulture, Action<DictionaryLoader> loader)
+            => app.UseLocalization(defaultCulture, new PathString("/assets/resources.js"), loader);
 
         /// <summary>
         /// Sets culture used by the application according to Accept-Language header.
@@ -26,9 +38,24 @@ namespace Microsoft.AspNet.Builder
         /// <param name="scriptPath">A path where to serialize a script with localized values.</param>
         /// <param name="loader">The dictionary loader.</param>
         public static IApplicationBuilder UseLocalization(this IApplicationBuilder app, PathString scriptPath, Action<DictionaryLoader> loader)
+            => app.UseLocalization("en-US", scriptPath, loader);
+
+        /// <summary>
+        /// Sets culture used by the application according to Accept-Language header.
+        /// </summary>
+        /// <param name="app">The application builder.</param>
+        /// <param name="defaultCulture">
+        /// The default culture used when a dictionary for the current culture does not exist.
+        /// </param>
+        /// <param name="scriptPath">A path where to serialize a script with localized values.</param>
+        /// <param name="loader">The dictionary loader.</param>
+        public static IApplicationBuilder UseLocalization(this IApplicationBuilder app, string defaultCulture, PathString scriptPath, Action<DictionaryLoader> loader)
         {
+            Requires.NotNull(defaultCulture, nameof(defaultCulture));
             Requires.NotNull(scriptPath, nameof(scriptPath));
             Requires.NotNull(loader, nameof(loader));
+
+            Resources.DefaultCulture = new CultureInfo(defaultCulture);
 
             return app
                 .UseMiddleware<CultureMiddleware>(loader)
--- src/Moon.Owin.Localization/Extensions/AppBuilder.cs
+++ src/Moon.Owin.Localization/Extensions/AppBuilder.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 using Microsoft.Owin;
 using Moon;
 using Moon.Localization;
@@ -17,7 +18,18 @@ namespace Owin
         /// <param name="app">The application builder.</param>
         /// <param name="loader">The dictionary loader.</param>
         public static IAppBuilder UseLocalization(this IAppBuilder app, Action<DictionaryLoader> loader)
-            => app.UseLocalization(new PathString("/assets/resources.js"), loader);
+            => app.UseLocalization("en-US", loader);
+
+        /// <summary>
+        /// Sets culture used by the application according to Accept-Language header.
+        /// </summary>
+        /// <param name="app">The application builder.</param>
+        /// <param name="defaultCulture">
+        /// The default culture used when a dictionary for the current culture does not exist.
+        /// </param>
+        /// <param name="loader">The dictionary loader.</param>
+        public static IAppBuilder UseLocalization(this IAppBuilder app, string defaultCulture, Action<DictionaryLoader> loader)
+            => app.UseLocalization(defaultCulture, new PathString("/assets/resources.js"), loader);
 
         /// <summary>
         /// Sets culture used by the application according to Accept-Language header.
@@ -26,10 +38,25 @@ namespace Owin
         /// <param name="scriptPath">A path where to serialize a script with localized values.</param>
         /// <param name="loader">The dictionary loader.</param>
         public static IAppBuilder UseLocalization(this IAppBuilder app, PathString scriptPath, Action<DictionaryLoader> loader)
+            => app.UseLocalization("en-US", scriptPath, loader);
+
+        /// <summary>
+        /// Sets culture used by the application according to Accept-Language header.
+        /// </summary>
+        /// <param name="app">The application builder.</param>
+        /// <param name="defaultCulture">
+        /// The default culture used when a dictionary for the current culture does not exist.
+        /// </param>
+        /// <param name="scriptPath">A path where to serialize a script with localized values.</param>
+        /// <param name="loader">The dictionary loader.</param>
+        public static IAppBuilder UseLocalization(this IAppBuilder app, string defaultCulture, PathString scriptPath, Action<DictionaryLoader> loader)
         {
+            Requires.NotNull(defaultCulture, nameof(defaultCulture));
             Requires.NotNull(scriptPath, nameof(scriptPath));
             Requires.NotNull(loader, nameof(loader));
-            
+
+            Resources.DefaultCulture = new CultureInfo(defaultCulture);
+
             return app
                 .Use<CultureMiddleware>(loader)
                 .Map(scriptPath, m =>
